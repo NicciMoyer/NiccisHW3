eea2f6d459eb7fcebebe51430416367243e49505
Nicci Moyer <niccihendrix@yahoo.com>
2020-07-24 08:44:28 -0500

    password generator w prompts

--- script.js
+++ script.js
@@ -6,73 +6,96 @@ var lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
 var upperAlpha = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
 var numVar = "0123456789";
 var specialChar = "!@#$%^&?";
+var builtString = ""
 
 var randomPassword = ""
 
+var inputLower = confirm("Do you want your password to contain lower case letters?")
+var inputUpper = confirm("Do you want your password to contain upper case letters?")
+var inputNum = confirm("Do you want your password to contain numbers?")
+var inputChar = confirm("Do you want your password to contain special characters?")
+var passwordLength = parseInt(prompt ("How many characters would you like your password to be?"))
+
+if (passwordLength < 8) {
+ alert ("Your password must be at least 8 charaters.")
+} 
+
+
+if (passwordLength > 128) {
+alert ("Your password must be less than 128 characters.")
+}
 
 
 
 
+function generatePassword() {
+
+  if (inputLower === true) {
+    builtString +=  lowerAlpha
+    
+  }
+
+  if (inputUpper === true) {
+      builtString += upperAlpha
+  }
+
+  if (inputNum === true){
+    builtString += numVar
+  }
+
+ if (inputChar === true) {
+   builtString += specialChar
+ }
 
 
-function generatePassword(passwordLength ) {
-
-
-  for (let i = 0; i < 8; i++) {
-    var randomLower = Math.floor(Math.random()*lowerAlpha.length);
-    randomPassword += lowerAlpha[randomLower]
+  for (let i = 0; i < passwordLength; i++) {
+    var randomLower = Math.floor(Math.random()*builtString.length);
+    randomPassword += builtString[randomLower]
   }
   // console.log(randomPassword)
   
-  for (let i  = 0; i < 8; i++) {
-    var randomUpper = Math.floor(Math.random()*upperAlpha.length);
-    randomPassword += upperAlpha[randomUpper]  
-  }   
+  // for (let i  = 0; i < 8; i++) {
+  //   var randomUpper = Math.floor(Math.random()*upperAlpha.length);
+  //   randomPassword += upperAlpha[randomUpper]  
+  // }   
+  // // console.log(randomPassword)
+  
+  
+  // for (let i  = 0; i < 8; i++) {
+  //   var randomNum = Math.floor(Math.random()*numVar.length);
+  //   randomPassword += numVar[randomNum]  
+  // }   
+  // // console.log(randomPassword)
+  
+
+  // for (let i  = 0; i < 12; i++) {
+  //   var randomChar = Math.floor(Math.random()*specialChar.length);
+  //   randomPassword += specialChar[randomChar]  
+  // }   
   // console.log(randomPassword)
-  
-  
-  for (let i  = 0; i < 8; i++) {
-    var randomNum = Math.floor(Math.random()*numVar.length);
-    randomPassword += numVar[randomNum]  
-  }   
-  // console.log(randomPassword)
-  
 
-  for (let i  = 0; i < 12; i++) {
-    var randomChar = Math.floor(Math.random()*specialChar.length);
-    randomPassword += specialChar[randomChar]  
-  }   
-  console.log(randomPassword)
+  // randomPassword += randomLower;
 
-  randomPassword += randomLower;
+  // randomPassword += randomUpper; 
 
-  randomPassword += randomUpper; 
-
-  randomPassword += randomNum; 
+  // randomPassword += randomNum; 
   
-  randomPassword += randomChar;
+  // randomPassword += randomChar;
 
   password = randomPassword; 
 
-
+  return password
 }
 
 function writePassword() {
   var password = generatePassword();
-  var randomPassword = document.querySelector("#password"); 
+  var passwordSpan = document.querySelector("#password"); 
 
-  randomPassword.value = password; 
+  passwordSpan.value = password; 
   
 }
 
 
-// if (passwordLength < 8) {
-//   return = "Your password must be at least 8 charaters."
-// } 
-
-// if (passwordLength > 128) {
-//   return = "Your password must be less than 128 characters."
-// }
 
 
 
