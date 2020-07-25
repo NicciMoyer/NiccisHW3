b11a1b5fe15edd71d4d2997abfcd80186478dae1
Nicci Moyer <niccihendrix@yahoo.com>
2020-07-24 20:44:28 -0500

    prompt text box change and readme created

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ var numVar = "0123456789";
 var specialChar = "!@#$%^&?";
 var builtString = ""
 
-var randomPassword = ""
-
-var inputLower = confirm("Do you want your password to contain lower case letters?")
-var inputUpper = confirm("Do you want your password to contain upper case letters?")
-var inputNum = confirm("Do you want your password to contain numbers?")
-var inputChar = confirm("Do you want your password to contain special characters?")
 var passwordLength = parseInt(prompt ("How many characters would you like your password to be?"))
+var inputLower = confirm("Do you want your password to contain lower case letters? Press OK for Yes. Press Cancel for No.")
+var inputUpper = confirm("Do you want your password to contain upper case letters? Press OK for Yes. Press Cancel for No.")
+var inputNum = confirm("Do you want your password to contain numbers? Press OK for Yes. Press Cancel for No.")
+var inputChar = confirm("Do you want your password to contain special characters? Press OK for Yes. Press Cancel for No.")
+
 
 if (passwordLength < 8) {
  alert ("Your password must be at least 8 charaters.")
@@ -40,6 +39,7 @@ function generatePassword() {
    builtString += specialChar
  }
 
+  var randomPassword = " "
   for (let i = 0; i < passwordLength; i++) {
     var randomLower = Math.floor(Math.random()*builtString.length);
     randomPassword += builtString[randomLower]
@@ -50,10 +50,11 @@ function generatePassword() {
   return password
 }
 
-function writePassword() {
-  var password = generatePassword();
+function writePassword(event) {
+  event.preventDefault()
   var passwordSpan = document.querySelector("#password"); 
-
+  passwordSpan.value = ""; 
+  var password = generatePassword();
   passwordSpan.value = password; 
 
 }
